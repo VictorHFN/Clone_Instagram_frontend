915bd09da9a3666c41c728b730fd2bbb10d5fd0a
Victor Hugo Nascimento <vhfn2004@gmail.com>
2022-03-28 15:06:16 -0300

    Frontend primeira parte

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.css'
+import { Link } from 'react-router-dom';
+
+import logo from '../assets/logo.svg'
+import camera from '../assets/camera.svg'
+
+export default Header;
+function Header() {
+  return(
+      <header id="main-header">
+         <div className="header-content">
+           <Link to="/">
+           <img src={logo} alt="InstaRocket" />
+           </Link>
+          <Link to="/new">
+           <img src={camera} alt="Enviar publicaçâo" />
+           </Link>
+         </div>
+      </header>
+  )
+  
+
+}
+ 
+
+
